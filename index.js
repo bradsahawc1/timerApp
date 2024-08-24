4a13242808f225989348481f4a928c861b9d78bb
cbradshaw <cnbradshaw24@gmail.com>
2024-08-23 21:07:17 -0400

    Added a check to make sure timer doesn't go below 0 and added log messages for when the timer is operating.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
+        if(this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.interval = setInterval(this.tick, 1000);
     };
@@ -18,7 +26,17 @@ class Timer {
     };
 
     tick = () => {
-        this.timeRemaining = this.timeRemaining - 1;
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete;
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick;
+            }
+        }
     };
 
     get timeRemaining() {
@@ -34,4 +52,14 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('Timer started');
+    },
+    onTick() {
+        console.log('Timer just ticked down');
+    },
+    onComplete() {
+        console.log('Timer is completed');
+    }
+}); 
